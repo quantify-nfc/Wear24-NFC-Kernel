b3a278f8603003f4ffe6b9378b39af651730c4f8
David Wheatley <blokmaster.help@gmail.com>
2019-02-01 21:58:15 +0000

    Add a build-start notification for Discord [ci-skip]

---
+++ kernel/out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 0 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#0 SMP PREEMPT Fri Feb 1 21:49:37 GMT 2019"
+#define LINUX_COMPILE_BY "david"
+#define LINUX_COMPILE_HOST "david-VirtualBox"
+#define LINUX_COMPILER "gcc version 4.8 (GCC) "
