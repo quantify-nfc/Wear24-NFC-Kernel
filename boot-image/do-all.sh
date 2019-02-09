cf46549d09f7ac50b7bc5222e3c1f6b3af34b94c
David Wheatley <blokmaster.help@gmail.com>
2019-02-09 22:15:39 +0000

    Add 'travis' to boot image name if created through TravisCI

--- boot-image/do-all.sh
+++ boot-image/do-all.sh
@@ -1,6 +1,11 @@
 export DATE_TIME=$(date +"%m-%d-%Y_%H-%M-%S")
 export branch=$(git symbolic-ref --short HEAD)
 
+if [ $CI == true ]
+then
+	export branch="-travis-"$branch
+fi
+
 ./extract-img.sh
 ./build-image.sh
 
@@ -8,5 +13,5 @@ echo "Deleting unneeded files..."
 rm -rf unpacked
 
 echo "Done!"
-echo "If build succeeded, find kernel image here: "$(pwd)"/JareDav-"$DATE_TIME"-"$branch".img"
+echo If build succeeded, find kernel image here: $(pwd)/JareDav-$DATE_TIME-$branch-.img
 echo "If build succeeded, find zImage here: "$(pwd)"/zImage-dtb"
