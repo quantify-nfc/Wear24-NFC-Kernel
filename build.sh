1b880a43d4b152e362b220515393abbc55240c99
Jared Tamana <seanythedream@gmail.com>
2019-02-08 22:01:19 -0500

    Make build script a bit... fancier ;)

--- build.sh
+++ build.sh
@@ -1,3 +1,24 @@
+#!/bin/bash
+timeout () {
+    tput sc
+    time=$1; while [ $time -ge 0 ]; do
+        tput rc; tput el
+        printf "$2" $time
+        ((time--))
+        sleep 1
+    done
+    tput rc; tput ed;
+}
+
+clear
+echo "Wear24 NFC kernel"
+echo "WIP by JaredTamana and davwheat (XDA)"
+echo "Thx to osm0sis (mkbootimg), lexri, bensdeals"
+echo "And Quanta/AOSP, I suppose :')"
+echo "---------------------------------------------------"
+timeout 3 "Build begins in %s seconds."
+
+
 cd kernel
 
 export CROSS_COMPILE=$(pwd)/../tools/toolchain/arm-eabi/bin/
