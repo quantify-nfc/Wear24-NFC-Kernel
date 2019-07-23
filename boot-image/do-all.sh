f3bbd0a7229b1af804bbc11ea309030496cd67fc
David Wheatley <blokmaster.help@gmail.com>
2019-07-23 17:38:20 +0100

    fix(do-all.sh): fix unary operator bug

--- boot-image/do-all.sh
+++ boot-image/do-all.sh
@@ -1,8 +1,7 @@
 export DATE_TIME=$(date +"%m-%d-%Y_%H-%M-%S")
 export branch=$(git symbolic-ref --short HEAD)
 
-if [ $CI == "true" ]
-then
+if [[ $CI==true ]]; then
 	export branch="-travis-"$TRAVIS_BRANCH
 else
 	export branch=$(git symbolic-ref --short HEAD)
