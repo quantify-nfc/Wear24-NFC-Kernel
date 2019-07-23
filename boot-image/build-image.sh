59b8adc24aab0b2aa89389e77513e3e910b844c0
David Wheatley <blokmaster.help@gmail.com>
2019-07-23 19:52:56 +0100

    Merge latest working release to master (#26)
    * Remove trace_printk() from kernel source (fix #13)
    
    * Fix broken deploy after name change
    
    * perf(trace_printk): removes traces of trace_printk
    
    * chore(bootimage): disable SELinux for now
    
    * chore(buildscripts): make unpack/repact more explicit [ci skip]
    
    * chore(do-all.sh): Fix "unary operator expected" in BASH
    
    * chore(name scheme): clarify that these are Quanitfy kernels
    
    * fix(SELinux): disable SELinux enforcing in Makefile
    
    * fix(#21): rearrange driver makefile to fix i2c/clk probing (#24)
    
    Fixes #21 by moving `clk` to top of makefile, as suggested by https://stackoverflow.com/questions/30166717/how-to-fix-error-on-clk-getcore-clk-during-probing-driver-i2c-msm-v2
    
    * feat(nfc): add PN544 drivers to defconfig
    
    * fix(dma-mapping): fix signed size_t error ob build
    
    Replaces "%zd" with "%zu" in a print statement within dma-mapping.c.
    The print statment only displays info or debugging so it could be
    removed, but it is better to keep it.
    
    `size_t` is almost always unsigned but this statement wanted a signed
    `size_t`. No clue why. Turns out %zd is printing as a signed decimal
    and %zu is an unsigned decimal, so I just swapped them out in the code.
    
    Source: https://stackoverflow.com/a/2524675/11091039
    
    * Revert "fix(dma-mapping): fix signed size_t error ob build"
    
    This reverts commit 3d5284999fd083cb0537214aa7508399ca007041.
    
    * Revert "feat(nfc): add PN544 drivers to defconfig"
    
    This reverts commit 219d3dcbda90309ceedd864dc0e7c9542ea9c9c5.
    
    * feat(PN544 drivers): add PN544 NFC chip drivers
    
    And it works this time...
    
    * fix(unneeded file): remove kernel/out/.config
    
    We don't need this and the file should have been ignored as shown
    in the .gitignore.
    
    * fix(defconfig): add missing nfc config entries

--- boot-image/build-image.sh
+++ boot-image/build-image.sh
@@ -1,1 +1,24 @@
-../tools/mkbootimg/mkbootimg --kernel zImage-dtb --ramdisk unpacked/stock.img-ramdisk.gz --cmdline "console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=dorado msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 buildvariant=user log_buf_len=8M" --base 80000000 --pagesize 2048 --kernel_offset 00008000 --ramdisk_offset 01000000 --tags_offset 00000100 --os_version 7.1.1 --os_patch_level 2017-03 -o Quantify-$DATE_TIME-$branch.img
+export kernel=zImage-dtb
+export ramdisk=unpacked/stock.img-ramdisk.gz
+export cmdline="console=ttyHSL0,115200,n8 enforcing=0 androidboot.selinux=permissive androidboot.console=ttyHSL0 androidboot.hardware=dorado msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 buildvariant=user log_buf_len=8M"
+export base=80000000
+export pagesize=2048
+export kernel_offset=00008000
+export ramdisk_offset=01000000
+export tags_offset=00000100
+export os_version=7.1.1
+export os_patch_level=2017-03
+
+echo "--------------------------------------------------"
+echo "Running mkbootimg using the following parameters:"
+echo "BOARD_KERNEL_CMDLINE $cmdline"
+echo "BOARD_KERNEL_BASE $base"
+echo "BOARD_PAGE_SIZE $pagesize"
+echo "BOARD_KERNEL_OFFSET $kernel_offset"
+echo "BOARD_RAMDISK_OFFSET $ramdisk_offset"
+echo "BOARD_TAGS_OFFSET $tags_offset"
+echo "BOARD_OS_VERSION $os_version"
+echo "BOARD_OS_PATCH_LEVEL $os_patch_level"
+
+../tools/mkbootimg/mkbootimg --kernel zImage-dtb --ramdisk unpacked/stock.img-ramdisk.gz --cmdline "console=ttyHSL0,115200,n8 enforcing=0 androidboot.selinux=permissive androidboot.console=ttyHSL0 androidboot.hardware=dorado msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 buildvariant=user log_buf_len=8M" --base 80000000 --pagesize 2048 --kernel_offset 00008000 --ramdisk_offset 01000000 --tags_offset 00000100 --os_version 7.1.1 --os_patch_level 2017-03 -o Quantify-Kernel-$DATE_TIME-$branch.img
+echo "--------------- end of mkbootimg -----------------"
